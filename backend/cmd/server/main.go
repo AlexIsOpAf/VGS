4ee6eef4d2029da72cfb49e28366706da4390d64
AlexIsOpAf <truearchy@gmail.com>
2024-09-28 15:54:12 +0100

    Sorted merge conflicts

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/vgs/api/services/v1/users"
@@ -60,45 +63,37 @@ func runHTTPServer() {
 		log.Fatalf("Failed to serve HTTP: %v", err)
 	}
 }
-import (
-    "database/sql"
-    "fmt"
-    "log"
-    "os"
 
-    _ "github.com/lib/pq"
-)
-
-func main() {
+func dbEntrance() {
 	flag.Parse()
 
 	// Run the gRPC and HTTP servers concurrently
 	go runGRPCServer()
 	runHTTPServer()
-    // Get database connection info from environment variables
-    dbHost := os.Getenv("DB_HOST")
-    dbPort := os.Getenv("DB_PORT")
-    dbUser := os.Getenv("DB_USER")
-    dbPassword := os.Getenv("DB_PASSWORD")
-    dbName := os.Getenv("DB_NAME")
+	// Get database connection info from environment variables
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
 
-    fmt.Println("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable")
-    // Build the connection string
-    connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-            dbHost, dbPort, dbUser, dbPassword, dbName)
+	fmt.Println("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable")
+	// Build the connection string
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
 
-    // Connect to the PostgreSQL database
-    db, err := sql.Open("postgres", connStr)
-    if err != nil {
-            log.Fatalf("Error opening database connection: %v", err)
-    }
-    defer db.Close()
+	// Connect to the PostgreSQL database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Error opening database connection: %v", err)
+	}
+	defer db.Close()
 
-    // Ping the database to check the connection
-    err = db.Ping()
-    if err != nil {
-            log.Fatalf("Error connecting to the database: %v", err)
-    }
+	// Ping the database to check the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
 
-    fmt.Println("Successfully connected to the PostgreSQL database!")
+	fmt.Println("Successfully connected to the PostgreSQL database!")
 }
